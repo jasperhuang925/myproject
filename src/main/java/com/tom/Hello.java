85c0f388860a5574daa347ec6dcab64908381245
HuangChingTing <jasperhuang985@gmail.com>
2019-02-01 21:59:21 +0900

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//      System.out.println("hello, world");
+//      new Person().hello();
+        Person p= new Person();
+        p.hello();
+    }
+}
