2c2a2c3df7768ca4e31eff0181bfda1b2db46247
HuangChingTing <jasperhuang985@gmail.com>
2019-02-01 23:01:06 +0900

    java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -6,5 +6,25 @@ public class Hello {
 //      new Person().hello();
         Person p= new Person();
         p.hello();
+
+        int age = 24;
+        char c = '我';
+        byte b = 120;
+        float weight =66.5f;
+        double height=172.6;
+        String name = "Jasper";
+        boolean adult = true;
+        boolean enroll = false;
+
+        //包裝類別，Wrapper class
+        Integer age2 = 13;
+        int age2_byte=age2.byteValue();
+        Character c2;
+
+
+
+
+
+
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -5,6 +5,12 @@ fun main(args: Array<String>) {
 //    Human().hello();
     val h = Human()
     h.hello()
+    //var variable 可改變 ; val 不可改變
+    val age =19;//變數型態的自動推斷
+    var weight =66.5;
+    weight=65.0;
+    var name : String;
+    name = "Jasper";
 }
 
 class Human {
